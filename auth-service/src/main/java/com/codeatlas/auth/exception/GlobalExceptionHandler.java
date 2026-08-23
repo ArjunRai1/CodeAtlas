b26d34f801cf2a106ffe927995fddd586dceec21
ArjunRai1 <arjunrai2214@gmail.com>
2026-08-23 08:29:30 +0530

    Global Exceptino handling

--- auth-service/src/main/java/com/codeatlas/auth/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/codeatlas/auth/exception/GlobalExceptionHandler.java
@@ -1,7 +1,23 @@
 package com.codeatlas.auth.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateEmailException(DuplicateEmailException ex, HttpServletRequest request) {
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
 }
